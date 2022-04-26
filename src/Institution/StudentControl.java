074e06cd6311e1711098f191a8cbc38dbab464fc
Mierley <mierer.lei@gmail.com>
2022-04-26 21:50:25 +0300

    Added Student Control (Zamira)

---
+++ src/Institution/StudentControl.java
@@ -0,0 +1,31 @@
+package Institution;
+
+import WorkWithDataBase.AttendanceDB;
+import WorkWithDataBase.GradeDB;
+import WorkWithDataBase.PaymentBookDB;
+
+import java.util.GregorianCalendar;
+
+public class StudentControl {
+    public void giveGrade(Student student,String subjectName, char mark){
+        Grade grade = new Grade(student.getID(), subjectName, mark, new GregorianCalendar().getTime());
+        GradeDB db = GradeDB.getInstance();
+        db.write(grade);
+        markClass(student, subjectName);
+    }
+
+    public void acceptPayment(Student student, double amount)
+    {
+        PaymentBookDB db = PaymentBookDB.getInstance();
+        PaymentBook paymentBook = db.load(student.getID()).stream().findFirst().get();
+        paymentBook.increaseInvestment(amount);
+        db.write(paymentBook);
+    }
+
+    public void markClass(Student student, String subjectName)
+    {
+        AttendanceDB db = AttendanceDB.getInstance();
+        Attendance attendance = new Attendance(student.getID(), subjectName, true,  new GregorianCalendar().getTime());
+        db.write(attendance);
+    }
+}
