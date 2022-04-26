1222fdc2f159fd4529a3e6d0f6927848cf32dea4
Toirov Sadi <sadi.toirov2003@gmail.com>
2022-04-26 04:59:14 +0300

    added implementation for load() and write()

--- src/WorkWithDataBase/GradeDB.java
+++ src/WorkWithDataBase/GradeDB.java
@@ -4,11 +4,13 @@ import Institution.Grade;
 import PatternVisitor.Element;
 import PatternVisitor.Visitor;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
 
 public class GradeDB implements WorkWithDataBase<Grade>, Element {
     private static GradeDB instance = null;
+    private final DataBase db = new DataBase();
 
     private GradeDB() {}
 
@@ -22,17 +24,30 @@ public class GradeDB implements WorkWithDataBase<Grade>, Element {
 
     @Override
     public Collection<Grade> load(int studentID) {
-        return null;
+        Collection<Grade> data = new ArrayList<>();
+        for(Grade grade: db.getGradeTable()){
+            if(grade.getStudentID() == studentID){
+                data.add(grade);
+            }
+        }
+        return data;
     }
 
     @Override
     public Collection<Grade> load(Date date) {
-        return null;
+        Collection<Grade> data = new ArrayList<>();
+        for(Grade grade: db.getGradeTable()){
+            if(grade.getDate().equals(date)){
+                data.add(grade);
+            }
+        }
+        return data;
     }
 
     @Override
     public void write(Grade newItem) {
-
+        db.removeGrade(newItem);
+        db.addGrade(newItem);
     }
 
     @Override
