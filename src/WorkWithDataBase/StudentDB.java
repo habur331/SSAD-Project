f702ae4b909fdbb10aa24836b0deedb1dc5ed69a
Toirov Sadi <sadi.toirov2003@gmail.com>
2022-04-26 05:01:08 +0300

    added implementation for load() and write()

--- src/WorkWithDataBase/StudentDB.java
+++ src/WorkWithDataBase/StudentDB.java
@@ -3,6 +3,7 @@ package WorkWithDataBase;
 import Institution.Student;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
 
@@ -10,34 +11,36 @@ public class StudentDB implements WorkWithDataBase<Student>
 {
 
     private static StudentDB instance = null;
+    private final DataBase db = new DataBase();
 
-    private StudentDB()
-    {
-    }
+    private StudentDB() {}
 
-    public static StudentDB getInstance()
-    {
+    public static StudentDB getInstance() {
         if (instance == null)
             instance = new StudentDB();
         return instance;
     }
 
     @Override
-    public Collection<Student> load(int studentID)
-    {
-        return null;
+    public Collection<Student> load(int studentID) {
+        Collection<Student> data = new ArrayList<>();
+        for(Student student: db.getStudentTable()){
+            if(student.getID() == studentID){
+                data.add(student);
+            }
+        }
+        return data;
     }
 
     @Override
-    public Collection<Student> load(Date date)
-    {
+    public Collection<Student> load(Date date) {
         // не нужно имплементировать
         throw new NotImplementedException();
     }
 
     @Override
-    public void write(Student newItem)
-    {
-
+    public void write(Student newItem) {
+        db.removeStudent(newItem);
+        db.addStudent(newItem);
     }
 }
